321676fb0e26d1a6f76d1bdd111be396acc0196d
Karisv <karis15@ru.is>
2016-05-10 22:19:33 +0000

    seinsta git eyddist ut hja mer implementadi thad aftur

--- Mooshak2/Models/ViewModels/UserViewModel.cs
+++ Mooshak2/Models/ViewModels/UserViewModel.cs
@@ -7,5 +7,10 @@ namespace Mooshak2.Models.ViewModels
 {
     public class UserViewModel
     {
+        public string userModel { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
     }
 }
